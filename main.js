ace9aca93e4455b4f4a8064ef20c1ce6df5c518c
Olivier Tille <olivier.tille@gmail.com>
2016-08-03 21:03:38 +0200

    updated main script to handle getBattery promise

--- main.js
+++ main.js
@@ -8,61 +8,70 @@
       display = doc.querySelector('.display'),
       jumbotron = doc.querySelector('.jumbotron'),
       sound = doc.querySelector('.sound'),
+      battery,
       WARNING_LEVEL = 10,
       DANGER_LEVEL = 4;
-  
+
   function msg(txt, status) {
     status = status || 'success';
     var p = doc.createElement('p');
     p.innerHTML = txt;
     display.appendChild(p);
   }
-  
-  if ('battery' in navigator) {
-    var bat = navigator.battery,
-      displayTime = function (timeInSec) {
-        var hours = Math.floor(timeInSec / 60 / 60),
-            minutes = Math.floor(timeInSec / 60) - Math.floor(timeInSec / 60 / 60) * 60,
-            seconds = 0;
-        return hours + 'h ' + minutes + 'm';
-      },
-      displayInfo = function() {
-        display.innerHTML = '';
-        msg('Your battery is <b>' + (bat.charging ? '' : 'dis') + 'charging</b>');
-        if (bat.chargingTime !== Infinity) {
-          msg('Charging Time: ' + displayTime(bat.chargingTime));
-        }
-        if (bat.dischargingTime !== Infinity) {
-          msg('Discharging Time: ' + displayTime(bat.dischargingTime));
-        }
-        msg('Level: ' + Math.round(bat.level * 100) + '%' );
-        displayLevelWarning();
-      },
-      displayLevelWarning = function() {
-        var level = Math.round(bat.level * 100);
-        if (level <= DANGER_LEVEL && !bat.charging) {
-          jumbotron.classList.remove('warning');
-          jumbotron.classList.add('danger');
-          msg('<b>Watch out your battey level is very low!</b>');
-          msg('You should really connect your power cord of your device now.');
-        } else if (level <= WARNING_LEVEL && !bat.charging) {
-          jumbotron.classList.remove('danger');
-          jumbotron.classList.add('warning');
-          msg('<b>Watch out your battey level is low!</b>');
-          msg('We\'d recommend connecting your power cord to your device now.');
-        } else {
-          jumbotron.classList.remove('danger');
-          jumbotron.classList.remove('warning');
-        }
-      };
-    
-    bat.onchargingchange = displayInfo;
-    bat.onchargingtimechange = displayInfo;
-    bat.ondischargingtimechange = displayInfo;
-    bat.onlevelchange = displayInfo;
-    
+
+  function displayTime(timeInSec) {
+    var hours = Math.floor(timeInSec / 60 / 60),
+        minutes = Math.floor(timeInSec / 60) - Math.floor(timeInSec / 60 / 60) * 60,
+        seconds = 0;
+    return hours + 'h ' + minutes + 'm';
+  }
+
+  function displayInfo() {
+    display.innerHTML = '';
+    msg('Your battery is <b>' + (battery.charging ? 'charging' : 'discharging') + '</b>.');
+    if (battery.charging && battery.chargingTime !== global.Infinity) {
+      msg('Time until fully charged: ' + displayTime(battery.chargingTime));
+    } else if (battery.charging) {
+      msg('Your battery is fully charged.');
+    } else if (battery.dischargingTime !== global.Infinity) {
+      msg('Time until fully discharged: ' + displayTime(battery.dischargingTime));
+    }
+    msg('Level: ' + (Math.round(battery.level * 10000) / 100) + '%' );
+    displayLevelWarning();
+  }
+
+  function displayLevelWarning() {
+    var level = Math.round(battery.level * 100);
+    if (level <= DANGER_LEVEL && !battery.charging) {
+      jumbotron.classList.remove('warning');
+      jumbotron.classList.add('danger');
+      msg('<b>Watch out your battey level is very low!</b>');
+      msg('You should really connect your power cord of your device now.');
+    } else if (level <= WARNING_LEVEL && !battery.charging) {
+      jumbotron.classList.remove('danger');
+      jumbotron.classList.add('warning');
+      msg('<b>Watch out your battey level is low!</b>');
+      msg('We\'d recommend connecting your power cord to your device now.');
+    } else {
+      jumbotron.classList.remove('danger');
+      jumbotron.classList.remove('warning');
+    }
+  }
+
+  function handleBattery(bat) {
+    battery = bat;
+    battery.addEventListener('chargingchange', displayInfo);
+    battery.addEventListener('chargingtimechange', displayInfo);
+    battery.addEventListener('dischargingtimechange', displayInfo);
+    battery.addEventListener('levelchange', displayInfo);
+
     displayInfo();
-    display.className += ' update';
+  }
+
+  if ('getBattery' in navigator) {
+    navigator.getBattery().then(handleBattery);
+  } else if ('batteyr' in navigator) {
+    handleBattery(navigator.battery);
   } else {
     msg('Sorry, your browser doesn\'t support the BatteryManager Web API.');
   }
